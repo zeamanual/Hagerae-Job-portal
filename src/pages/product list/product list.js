542c549d0f428e08a9d7d1bec937faab2733d025
abe <zeamanualfeleke@gmail.com>
2022-03-09 17:10:11 +0300

    basic folder structure created

---
+++ src/pages/product list/product list.js
@@ -0,0 +1,40 @@
+import React,{useState,useEffect} from 'react'
+import Product from '../../components/product/product'
+
+import useFetch from '../../hooks/useFetch'
+import './product list.css'
+
+function  ProductList() {
+  let productsUrl = "https://fakestoreapi.com/products"
+
+  let {loading,data,error}=useFetch(productsUrl)
+
+  if(loading){
+    return <h2>Loading ...</h2>
+  }
+  else if(error.errorOccured){
+    return<h2>Error Occured...{`\n ${error.errorMessage}`}</h2>
+  }
+  else {
+      return (
+        <div className='products'>
+        <h2 className='title'>Product List</h2>
+        <div className='product-list'>
+        {
+          data.map(product=>{
+            return <Product key={product.id} title={product.title} img={product.image}
+             price={product.price} rating={product.rating.rate} 
+             count={product.rating?.count}
+                  ></Product>
+          })
+        }
+        </div>
+
+        </div>
+    )
+  }
+
+
+}
+
+export default ProductList
